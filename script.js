d4a467a556db137b3438c99aa999574b5cf6cb99
petark7 <kuzmanovski.petar@uklo.edu.mk>
2022-10-31 21:22:28 +0100

    Write playRound function

--- script.js
+++ script.js
@@ -4,17 +4,54 @@ function getComputerChoice () {
     switch (result)
     {
         case 1:
-            return "Rock";
+            return "ROCK";
             break;
         case 2:
-            return "Paper";
+            return "PAPER";
             break;
         case 3:
-            return "Scissors"
+            return "SCISSORS"
             break;
     }
 }
 
 function playRound (playerSelection, computerSelection) {
-    
+    if (playerSelection == "ROCK" && computerSelection == "ROCK")   {
+        console.log(`It's a tie! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+        return "tie";
+    }  
+        else if ((playerSelection == "ROCK" && computerSelection == "SCISSORS"))  {
+            console.log(`You won! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+            return "win";
+        }
+        else if ((playerSelection == "ROCK" && computerSelection == "PAPER")) {
+            console.log(`You lost! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+            return "lose";
+        }
+
+    if (playerSelection == "PAPER" && computerSelection == "PAPER")   {
+        console.log(`It's a tie! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+        return "tie";
+    }  
+        else if ((playerSelection == "PAPER" && computerSelection == "ROCK"))  {
+            console.log(`You won! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+            return "win";
+        }
+        else if ((playerSelection == "PAPER" && computerSelection == "SCISSORS")) {
+            console.log(`You lost! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+            return "lose";
+        }
+
+    if (playerSelection == "SCISSORS" && computerSelection == "SCISSORS")   {
+        console.log(`It's a tie! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+        return "tie";
+    }  
+        else if ((playerSelection == "SCISSORS" && computerSelection == "PAPER"))  {
+            console.log(`You won! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+            return "win";
+        }
+        else if ((playerSelection == "SCISSORS" && computerSelection == "ROCK")) {
+            console.log(`You lost! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+            return "lose";
+        }
 }
