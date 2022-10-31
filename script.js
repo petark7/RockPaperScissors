b58337defff387cf38969d16e1f594c5f753c497
petark7 <kuzmanovski.petar@uklo.edu.mk>
2022-10-31 23:11:24 +0100

    Update code with UI

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var scorePlayer = 0;
 var scoreComputer = 0;
 
+var results = document.getElementById("results");
+var score = document.getElementById("score");
+var gameResults = document.getElementById("gameResults");
+
+score.textContent = 0;
 function getComputerChoice () {
     let result = Math.ceil(Math.random() * 3); //3 is the max number
 
@@ -18,77 +23,82 @@ function getComputerChoice () {
     }
 }
 
+
 // Plays a round and returns either tie, win or lose (to be used iin game funct)
 function playRound (playerSelection, computerSelection) {
 
     if (playerSelection == "ROCK" && computerSelection == "ROCK")   {
-        console.log(`It's a tie! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+        results.innerText = (`It's a tie! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
         return "tie";
     }  
     else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
-        console.log(`You lost! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+        results.innerText =(`You lost! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
         return "lose";
     }
         else if (playerSelection == "ROCK" && computerSelection == "SCISSORS")  {
-            console.log(`You won! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+            results.innerText = (`You won! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
             return "win";
         }
       
 
     if (playerSelection == "PAPER" && computerSelection == "PAPER")   {
-        console.log(`It's a tie! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+        results.innerText = (`It's a tie! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
         return "tie";
     }  
         else if (playerSelection == "PAPER" && computerSelection == "ROCK")  {
-            console.log(`You won! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+            results.innerText = (`You won! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
             return "win";
         }
         else if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
-            console.log(`You lost! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+            results.innerText = (`You lost! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
             return "lose";
         }
 
     if (playerSelection == "SCISSORS" && computerSelection == "SCISSORS")   {
-        console.log(`It's a tie! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+        results.innerText = (`It's a tie! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
         return "tie";
     }  
         else if (playerSelection == "SCISSORS" && computerSelection == "PAPER")  {
-            console.log(`You won! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+            results.innerText = (`You won! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
             return "win";
         }
         else if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
-            console.log(`You lost! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
+            results.innerText = (`You lost! You chose ${playerSelection}, and the computer chose ${computerSelection}.`);
             return "lose";
         }
 }
 
-function game () {
-    for (let i = 0; i < 5; i++) {
-        let userInput = prompt("Rock, paper or scissors?");
+function game (userInput) {
         let computerChoice = getComputerChoice(); // wrote variable not to get different result each time if is called
         let results = playRound(userInput.toUpperCase(), computerChoice);
         
         if (results == "win")
         {
             scorePlayer += 1;
+
         }
         else if (results == "lose")
         {
             scoreComputer += 1;
         }
-    }
 
-    console.log(`Final score is: player = ${scorePlayer}, computer = ${scoreComputer}`);
-    if (scorePlayer > scoreComputer)
-    {
-        console.log("You won!");
+        score.textContent = `Player ${scorePlayer} : ${scoreComputer} Computer`;
+
+        if (scorePlayer == 5 || scoreComputer == 5) {
+        console.log(`Final score is: player = ${scorePlayer}, computer = ${scoreComputer}`);
+        if (scorePlayer > scoreComputer)
+        {
+            gameResults.textContent = ("You won!");
+        }
+            else if (scoreComputer > scorePlayer) {
+                gameResults.textContent = ("You lost :(");
+            }
+            else {
+                gameResults.textContent = ("It's a tie!");
+            }
     }
-    else if (scoreComputer > scorePlayer) {
-        console.log ("You lost :(");
-    }
-    else {
-        console.log ("It's a tie!");
-    }
-    scorePlayer = 0;
-    scoreComputer = 0;
+}
+
+function buttonClick (userInput) {
+    game (userInput);
 }
